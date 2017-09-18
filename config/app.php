b3051883963c26615dc91f7878c97715e02c5b4d
Skeepy <skeepy.ru@gmail.com>
2017-09-19 04:35:48 +0500

    Revert "Revert "Добавлен каркас фреймворка""

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skeepy
+ * Date: 19.09.2017
+ * Time: 1:02
+ */
+
+
+/**
+ * Файл конфигурации приложения
+ */
+
+// Имя приложения
+const AppName = 'Testing Framework';
